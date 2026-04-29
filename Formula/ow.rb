10b8dbfb116b232029533cb2ca674dab11f0306c
Marlon Raskin <48145861+marlonjames71@users.noreply.github.com>
2026-04-29 04:14:35 -0400

    ow 0.3.0
    Update OW formula for 0.3.0.

--- Formula/ow.rb
+++ Formula/ow.rb
@@ -1,8 +1,8 @@
 class Ow < Formula
   desc "Manage macOS Open With defaults from the command-line"
   homepage "https://github.com/marlonjames71/OW-CLI"
-  url "https://github.com/marlonjames71/OW-CLI/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "48e27c36f4535219e9fbe0b246dcfe15941086258ccb9739313e420f11f8567c"
+  url "https://github.com/marlonjames71/OW-CLI/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "64c20b0a6377d7976c0ce18efe8e25aa06de4b305f5127534b7e0eb965164fac"
   license "MIT"
 
   depends_on xcode: ["14.3", :build]
